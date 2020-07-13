db1d8c7b70c861a701e490173750ff8a0c932b1c
WebIoT.id <34671621+ahroihan@users.noreply.github.com>
2020-07-14 04:31:52 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; ">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Master-PhpActiveRecord-SSP</title>
+  </head>
+  <body>
+    <?php
+      echo '<img src="https://webiot.id/ssp1.png" alt="Image" style="display: block;margin: 0 auto;max-height: 100%;max-width: 100%;width: 100%;" />';
+    ?>
+  </body>
+</html>
